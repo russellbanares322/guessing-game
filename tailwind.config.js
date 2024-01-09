4df98b7fba321ae4fbea3a3742bec746f9a307f5
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-01-09 21:10:51 +0800

    feat: add custom colors in tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#132531",
+        "ghost-blue": "#BCCEDA",
+        "light-blue": "#31485A",
+        orange: "#FFA114",
+      },
+    },
   },
   plugins: [],
 };
