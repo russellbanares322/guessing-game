ebf8927abf7e23ef885fd9d9feb7e7129df221ae
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-01-10 22:11:26 +0800

    feat: create select number function that will save the selected number in array

--- src/components/FourByFourGridDisplay.tsx
+++ src/components/FourByFourGridDisplay.tsx
@@ -3,26 +3,106 @@ import { fourByFourGridData } from "../data/gridDatas";
 import { GridLayout } from "../layout";
 import { randomIndexGenerator } from "../utils/randomIndexGenerator";
 
-//*TODO: Create a separate grid renderer layout that can be reused at any component
+type SavedGridNumbers = {
+  id: number;
+  selectedNumber: number;
+};
+
+type TGridOptions = {
+  selectedGridNumber: SavedGridNumbers[] | [];
+  matchedNumbers: SavedGridNumbers[] | [];
+};
 
 const FourByFourGridDisplay = () => {
-  const [grid] = useState(fourByFourGridData);
+  const [gridOptions, setGridOptions] = useState<TGridOptions>({
+    selectedGridNumber: [],
+    matchedNumbers: [],
+  });
+  const hasTwoSelectedGridNumber = gridOptions.selectedGridNumber.length === 2;
+
+  const clearSelectedGridNumberArr = () => {
+    const selectedGridNumberRemoverTimeout = setTimeout(() => {
+      setGridOptions({
+        ...gridOptions,
+        selectedGridNumber: [],
+      });
+    }, 2000);
+
+    return () => clearTimeout(selectedGridNumberRemoverTimeout);
+  };
+
+  const selectNumber = (selectedNumberData: SavedGridNumbers) => {
+    const selectedGridNumberArr = gridOptions.selectedGridNumber;
+    setGridOptions({
+      ...gridOptions,
+      selectedGridNumber: [...selectedGridNumberArr, selectedNumberData],
+    });
+
+    if (hasTwoSelectedGridNumber) {
+      const firstSelectedGridNumber = selectedGridNumberArr[0].selectedNumber;
+      const secondSelectedGridNumber = selectedGridNumberArr[1].selectedNumber;
+      const gridNumbersMatched =
+        firstSelectedGridNumber === secondSelectedGridNumber;
+
+      if (!gridNumbersMatched) {
+        clearSelectedGridNumberArr();
+      }
+      setGridOptions({
+        ...gridOptions,
+        matchedNumbers: [
+          ...gridOptions.matchedNumbers,
+          ...selectedGridNumberArr,
+        ],
+      });
+    }
+  };
+
+  const showGridNumber = (
+    selectedNumberIndex: number,
+    selectedNumber: number
+  ) => {
+    const isGridNumberPresent = gridOptions.matchedNumbers.some(
+      (num) =>
+        num.id === selectedNumberIndex && num.selectedNumber === selectedNumber
+    );
+    const isGridNumberSelected = gridOptions.selectedGridNumber.some(
+      (num) =>
+        num.id === selectedNumberIndex && num.selectedNumber === selectedNumber
+    );
+
+    if (isGridNumberPresent || isGridNumberSelected) {
+      return selectedNumber;
+    }
+
+    return "";
+  };
 
   return (
-    <GridLayout flexDirection="col">
-      {grid?.map((row, rowIndex) => (
-        <GridLayout flexDirection="row" key={randomIndexGenerator(rowIndex)}>
-          {row.map((col, colIndex) => (
-            <div
-              className="bg-dark-blue p-3 rounded-full h-16 w-16 text-center text-white text-3xl"
-              key={randomIndexGenerator(colIndex)}
-            >
-              {col}
-            </div>
-          ))}
-        </GridLayout>
-      ))}
-    </GridLayout>
+    <>
+      {JSON.stringify(gridOptions)}
+      <GridLayout flexDirection="col">
+        {fourByFourGridData?.map((row, rowIndex) => (
+          <GridLayout flexDirection="row" key={randomIndexGenerator(rowIndex)}>
+            {row.map((number, colIndex) => {
+              const selectedNumberData = {
+                id: colIndex,
+                selectedNumber: number,
+              };
+              return (
+                <button
+                  disabled={hasTwoSelectedGridNumber}
+                  onClick={() => selectNumber(selectedNumberData)}
+                  className="bg-dark-blue p-3 rounded-full h-16 w-16 text-center text-white text-3xl cursor-pointer"
+                  key={randomIndexGenerator(colIndex)}
+                >
+                  {showGridNumber(colIndex, number)}
+                </button>
+              );
+            })}
+          </GridLayout>
+        ))}
+      </GridLayout>
+    </>
   );
 };
 
